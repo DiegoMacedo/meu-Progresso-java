de6d3482c0ab17d2791c9ae67b3a01bd72767198
oliveiramacedao@gmail.com <oliveiramacedao@gmail.com>
2025-09-12 20:18:35 -0400

    feat: adiciona exercicios de semaforo com else-if

---
+++ src/Semaforo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Semaforo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe o estado do semáforo:");
+        String estado = leitor.nextLine();
+        if(estado.equals("verde")){
+            System.out.println("O semáforo está verde, você pode passar");
+        }else if(estado.equals("amarelo")){
+            System.out.println("O semáforo está amarelo, atenção");
+        }else if(estado.equals("vermelho")){
+            System.out.println("O semáforo está vermelho, pare");
+        }else {
+            System.out.println("Estado inválido");
+        }
+        leitor.close();
+    }
+}
--- src/VerificadorDeIdade.java
+++ src/VerificadorDeIdade.java
@@ -4,14 +4,15 @@ public class VerificadorDeIdade {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Por favor, digite sua idade: ");
+        System.out.println("Informe a sua idade: ");
         int idade = leitor.nextInt();
 
-       if(idade >= 18){
-           System.out.println("Você é maior de idade!");
-       }else {
-           System.out.println("Você é menor de idade!");
-       }
+        if(idade >= 18){
+            System.out.println("Você está apto a tirar a carteira de motorista");
+        }else {
+            System.out.println("Você não está apto a tirar a carteira de motorista");
+        }
+        System.out.println("Você tem " + idade + " anos");
         leitor.close();
     }
 }
