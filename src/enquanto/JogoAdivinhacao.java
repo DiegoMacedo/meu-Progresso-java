ba4c7c582555ed0f13f4df261a48f00e4a79d5fb
oliveiramacedao@gmail.com <oliveiramacedao@gmail.com>
2025-09-15 21:48:15 -0400

    feat: Melhorias na lógica e implementação do jogo da adininhação

--- src/enquanto/JogoAdivinhacao.java
+++ src/enquanto/JogoAdivinhacao.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class JogoAdivinhacao {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
         int numeroSecreto = 42;
         int palpite = 0;
 
-        System.out.println("Jogo da Adivinhação");
-        System.out.println("Tente acertar o número entre (0 e 100)");
+        Scanner teclado = new Scanner(System.in);
 
-        while (palpite != numeroSecreto){
-            System.out.println("Digite seu palpite: ");
-            palpite = leitor.nextInt();
+        System.out.println("------Jogo Da Adivinhação------");
+        System.out.println("Tente encontrar um número entre (0 e 100) ");
 
-            if (palpite == numeroSecreto){
-                System.out.println("Parabéns, você ACERTOU!!");
+        while (palpite  != numeroSecreto){
+            System.out.println("Digite um valor entre (0 e 100) ");
+            palpite = teclado.nextInt();
+
+            if(palpite < 0 || palpite > 100){
+                System.out.println("Ops, intervalo inválido!");
+            }else if (palpite < numeroSecreto){
+                System.out.println("Aumente o seu palpite");
+            }else if(palpite > numeroSecreto){
+                System.out.println("Diminua seu palpite");
             }else{
-                System.out.println("Que pena, você ERROU!!! TENTE NOVAMENTE");
+                System.out.println("Parabéns, você acertou!!!");
             }
         }
-
-        System.out.println("FIM DO JOGO!!!");
-        leitor.close();
-
+        System.out.println("GAME OVER!!!");
+        teclado.close();
     }
 }
